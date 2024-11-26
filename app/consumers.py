aed48359480725adebf663a5116253c79f7116c9
AhnJoonSung <tjdtna01@naver.com>
2024-11-27 02:02:43 +0900

    :hammer: refactor: Consumer 클래스의 비즈니스 로직 services로 분리
    - 뚱뚱하던 Consumer::receive() 함수의 로직을 service 계층으로 분리
    - 유효성 검증 로직 추가

--- app/consumers.py
+++ app/consumers.py
@@ -1,18 +1,21 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth import get_user_model
-from .models import ChatRoom, Message
+from .models import Message
 import json
 from . import services
 
 User = get_user_model()
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def send_json(self, content):
-        await self.send(text_data=json.dumps(content))
-    
+class ChatConsumer(AsyncWebsocketConsumer):    
     async def connect(self):
+        # NOTE: 인증된 user_id 가져와서 채팅방에 접근가능한지 검사해야 함.
         self.room_name = self.scope['url_route']['kwargs']['chatroom_id']
         self.room_group_name = f'chat_{self.room_name}'
+        
+        self.chatroom = await services.get_chatroom_by_id(self.room_name)
+        if not self.chatroom:
+            await self.close()
+            return
         
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -21,7 +24,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         
         await self.accept()
         
-    async def disconnect(self, close_code):
+    async def disconnect(self):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -30,30 +33,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         
-        sender_id = data['sender_id']
-        if not services.is_valid_user(sender_id):
-            await self.send_json({
-                "type": "error",
-                "content": "Invalid sender ID."
-            })
+        error_response = await services.validate_message(data, self.chatroom)
+        if error_response:
+            await self.send_json(error_response)
             return
         
-        chatroom_id = data['chatroom_id']
+        sender_id = data['sender_id']
         content = data['content']
-        
-        chatroom = await ChatRoom.objects.filter(id=chatroom_id).afirst()
-        receiver_id = chatroom.get_receiver_id(sender_id)
-        
+        receiver_id = self.chatroom.get_receiver_id(sender_id)
+
         message = await Message.objects.acreate(
-            chatroom=chatroom,
+            chatroom=self.chatroom,
             sender_id=sender_id,
             receiver_id=receiver_id,
             content=content
         )
-        
-        chatroom.last_message = content
-        await chatroom.asave()
-        
+
+        self.chatroom.last_message = content
+        await self.chatroom.asave()
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -75,3 +73,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             "content": message_content,
             "timestamp": timestamp
         })
+        
+    async def send_json(self, content):
+        await self.send(text_data=json.dumps(content))
--- app/services.py
+++ app/services.py
@@ -34,3 +34,45 @@ def validate_users(user1_id, user2_id):
     if not is_valid_user(user2_id):
         return False
     return True
+
+async def get_chatroom_by_id(chatroom_id):
+    return await ChatRoom.objects.filter(id=chatroom_id).afirst()
+
+async def is_user_in_chatroom(user_id, chatroom):
+    return user_id in [chatroom.user1_id, chatroom.user2_id]
+
+async def validate_message(data, chatroom):
+    # NOTE: 인증된 user_id로 검사하게 수정해야 함.
+    sender_id = data.get('sender_id')
+    chatroom_id = data.get('chatroom_id')
+    content = data.get('content')
+
+    if missing_fields := check_missing_fields(sender_id, chatroom_id, content):
+        return error_response("Missing required fields: " + ", ".join(missing_fields))
+
+    if not is_valid_user(sender_id):
+        return error_response("Invalid sender ID.")
+
+    if int(chatroom_id) != chatroom.id:
+        return error_response("Chatroom ID mismatch.")
+
+    if not is_user_in_chatroom(sender_id, chatroom):
+        return error_response("Sender is not a participant in the chatroom.")
+
+    return None
+    
+def error_response(message):
+    return {
+        "type": "error",
+        "content": message
+    }
+    
+def check_missing_fields(sender_id, chatroom_id, content):
+    missing_fields = []
+    if not sender_id:
+        missing_fields.append('sender_id')
+    if not chatroom_id:
+        missing_fields.append('chatroom_id')
+    if not content:
+        missing_fields.append('content')
+    return missing_fields
