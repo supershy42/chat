0378d62b41e990f0910f57060b605ded415473a3
sujin Park <chemd78@naver.com>
2024-12-22 22:10:58 +0900

    :sparkles: feat: user profile 조회 API 호출 메서드 추가

---
+++ config/services.py
@@ -0,0 +1,12 @@
+import aiohttp
+from config.settings import USER_SERVICE
+
+async def get_user(user_id, token):
+    request_url = f'{USER_SERVICE}profile/{user_id}/'
+    headers = {'Authorization': f'Bearer {token}'}
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(request_url, headers=headers, timeout=10) as response:
+            if response.status == 200:
+                return await response.json()
+            return None 
